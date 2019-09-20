eab53ce97b8036201e036116f7be465cc507e33c
anushasingh30 <anusha2429@gmail.com>
2019-09-19 20:16:04 -0700

    BaseClient documentation added

--- backend/actors/client.py
+++ backend/actors/client.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
 
 class BaseClient(ABC):
+	"""
+	actors.client.BaseClient: it is an Abstract Base Class which represents an entity that interacts with the web-interface.
+
+	raises NotImplementedError on initialization. 
+	"""
 	def __init__(self):
 		raise NotImplementedError('Cannot initialize Abstract Class')
