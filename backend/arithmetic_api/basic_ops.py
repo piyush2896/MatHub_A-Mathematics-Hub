6abf8eaa0bc20aeafd11da10ae9e4376c70268c9
anushasingh30 <anusha2429@gmail.com>
2019-09-19 21:25:12 -0700

    Basic Operations defined

---
+++ backend/arithmetic_api/basic_ops.py
@@ -0,0 +1,11 @@
+def add(data):
+	print('Add Function:')
+
+def sub(data):
+	print('Subtract Function:')
+
+def mul(data):
+	print('Multiply Function:')
+
+def div(data):
+	print('Divide Function:')
