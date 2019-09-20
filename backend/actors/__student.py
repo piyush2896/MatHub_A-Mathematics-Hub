7a37d7f5500830b49ff780626abcddd33d766505
piyush2896 <piyushmalhotra28@gmail.com>
2019-09-19 20:02:23 -0700

    Added getters and setters

--- backend/actors/__student.py
+++ backend/actors/__student.py
@@ -7,3 +7,21 @@ class Student(BaseClient):
 		self.__name = name
 		self.__grade = grade
 		self.__marks = []
+
+	def set_name(self, name):
+		self.__name = name
+
+	def set_grade(self, grade):
+		self.__grade = grade
+
+	def add_marks(self, marks):
+		self.__marks.append(marks)
+
+	def get_name(self):
+		return self.__name
+
+	def get_grade(self):
+		return self.__grade
+
+	def get_marks(self):
+		return self.__marks
