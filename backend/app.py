bb2d712e36743a41f510e5880666aa76915f09ac
anushasingh30 <anusha2429@gmail.com>
2019-09-20 20:34:26 -0700

    Structure for mul and div

--- backend/app.py
+++ backend/app.py
@@ -10,5 +10,13 @@ def arithmetic_add():
 def arithmetic_sub():
     return flask.render_template_string('HEllO from sub')
 
+@app.route('/mul')
+def arithmetic_mul():
+	return flask.render_template_string('HEllO from mul')
+
+@app.route('/div')
+def arithmetic_div():
+	return flask.render_template_string('HEllO from div')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
