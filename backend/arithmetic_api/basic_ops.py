dc7308575e2c10e769a5e015cbe791f92646b269
piyush2896 <piyushmalhotra28@gmail.com>
2019-09-24 16:14:07 -0700

    Add Operator Implementor

--- backend/arithmetic_api/basic_ops.py
+++ backend/arithmetic_api/basic_ops.py
@@ -1,3 +1,31 @@
+class OperatorImplementor:
+	__instance = None
+
+	@staticmethod
+	def create():
+		if OperatorImplementor.__instance == None:
+			OperatorImplementor()
+		return OperatorImplementor.__instance
+
+	def __init__(self):
+		if OperatorImplementor.__instance != None:
+			raise Exception('Cannot re-instantiate a singleton class.')
+		
+		self.__ops_to_func = {
+			'+': add,
+			'-': sub,
+			'*': mul,
+			'/': div
+		}
+		self.__ops_available = set(self.__ops_to_func.keys())
+		OperatorImplementor.__instance = self
+
+	def do_op(self, data):
+		if data['operand'] not in self.__ops_available:
+			raise NotImplementedError('Operator not implemented')
+		else:
+			return self.__ops_to_func[data['operand']](data)
+
 def add(data):
 	"""
 	arithmetic_api.add: Adds two operands
