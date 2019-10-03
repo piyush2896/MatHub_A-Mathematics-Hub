7f54e142aa3e7c4ed969b552ba643684bfe2f91b
anushasingh30 <anusha2429@gmail.com>
2019-10-03 16:38:06 -0700

    Allow CORS

--- backend/app.py
+++ backend/app.py
@@ -25,5 +25,12 @@ def basic_arithmetic_ops():
     result = implementor.do_op(data)
     return flask.jsonify({'result': result})
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
