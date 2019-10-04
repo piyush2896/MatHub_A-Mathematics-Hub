a7614d2fef4198ee556a94eefc2cc72bba3072d5
anushasingh30 <anusha2429@gmail.com>
2019-10-03 18:30:17 -0700

    Create Arithmetic Board

--- tmp/app.py
+++ tmp/app.py
@@ -10,5 +10,9 @@ def index():
 def boards():
     return flask.render_template('boards.html')
 
+@app.route('/arithmetic_board')
+def arithmetic_board():
+    return flask.render_template('arith.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
