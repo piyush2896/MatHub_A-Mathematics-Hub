4faf1f843dbc34b7839454bd5c9d16566c70507d
anushasingh30 <anusha2429@gmail.com>
2019-10-03 18:12:02 -0700

    Create Index template

--- tmp/app.py
+++ tmp/app.py
@@ -2,5 +2,9 @@ import flask
 
 app = flask.Flask(__name__)
 
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
