4f203fde6cd9e47d5bf20bf24ee6d33c236012f3
anushasingh30 <anusha2429@gmail.com>
2019-10-03 18:24:48 -0700

    Create boards page

--- tmp/app.py
+++ tmp/app.py
@@ -6,5 +6,9 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template('index.html')
 
+@app.route('/boards')
+def boards():
+    return flask.render_template('boards.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
