cf3d9f23a06d6973c7306b86b9c3538818b80f72
piyush2896 <piyushmalhotra28@gmail.com>
2019-10-09 15:38:38 -0700

    Declare verification helper functions

--- backend/credentials/login_helper.py
+++ backend/credentials/login_helper.py
@@ -1,4 +1,11 @@
 def verify(username, password):
     # TODO: Find Username in DB
     # TODO: Check if Password Matches
- 
+
+def is_username(username):
+    # TODO: Find is username present and return bool
+    return True 
+
+def is_password_correct(password_candidate, password):
+    # TODO: Find if password is correct for given username
+    return True
