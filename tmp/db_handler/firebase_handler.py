9e118b08e59c593a6f947a6a5ac592386380208d
anushasingh30 <anusha2429@gmail.com>
2019-10-11 03:49:53 -0700

    exception handling

--- tmp/db_handler/firebase_handler.py
+++ tmp/db_handler/firebase_handler.py
@@ -4,9 +4,18 @@ import time
 import requests
 
 def add_data(firebase_url, data, log_file=None):
-    result = requests.post(firebase_url, data=json.dumps(data))
-    if log_file != None:
-        with open(log_file, 'a') as f:
-            f.write(time.strftime('%m-%d-%Y') + '|' +
-                    time.strftime('%H:%M:%S') +
-                    'Data Created: ' + json.dumps(data)+'\n')
+    try:
+        result = requests.post(firebase_url, data=json.dumps(data))
+        print(result.status_code, result.text)
+        # time.sleep(10)
+        if log_file != None:
+            with open(log_file, 'a') as f:
+                f.write(time.strftime('%m-%d-%Y') + '|' +
+                        time.strftime('%H:%M:%S') +
+                        'Data Created: ' + json.dumps(data)+'\n')
+    except Exception as e:
+        if log_file != None:
+            with open(log_file, 'a') as f:
+                f.write(time.strftime('%m-%d-%Y') + '|' +
+                        time.strftime('%H:%M:%S') +
+                        'Exception:' + str(e) + '\n')
