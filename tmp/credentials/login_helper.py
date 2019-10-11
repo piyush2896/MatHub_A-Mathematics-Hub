d777cca2134ebe3398a9e28aa03fa341c85b9d69
anushasingh30 <anusha2429@gmail.com>
2019-10-11 04:25:26 -0700

    Add username checker

--- tmp/credentials/login_helper.py
+++ tmp/credentials/login_helper.py
@@ -1,13 +1,15 @@
 from passlib.hash import sha256_crypt
 
-def verify(db_instance, username, password):
+from db_handler import firebase_handler as fb_handle
+
+def verify(db_url, username, password):
     # TODO: Find Username in DB
     # TODO: Check if Password Matches
-    return True
-
-def is_username(db_instance, username):
-    # TODO: Find is username present and return bool
     return True 
+
+def is_username(db_url, username):
+    data = fb_handle.retrieve_data(db_url, username)
+    return data != None
 
 def is_password_correct(password_candidate, password):
     return sha256_crypt.verify(password_candidate, password)
