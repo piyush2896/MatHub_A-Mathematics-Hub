729c98fafc52fea65e0645f66029f0648bcf9081
piyush2896 <piyushmalhotra28@gmail.com>
2019-10-11 03:52:06 -0700

    Remove Print logs

--- tmp/db_handler/firebase_handler.py
+++ tmp/db_handler/firebase_handler.py
@@ -6,8 +6,6 @@ import requests
 def add_data(firebase_url, data, log_file=None):
     try:
         result = requests.post(firebase_url, data=json.dumps(data))
-        print(result.status_code, result.text)
-        # time.sleep(10)
         if log_file != None:
             with open(log_file, 'a') as f:
                 f.write(time.strftime('%m-%d-%Y') + '|' +
