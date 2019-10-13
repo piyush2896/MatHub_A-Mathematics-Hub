a0b827826098fe69a1fcc2ff25b2f9b36a448fd8
Amudhan <amudhanmanisekaran@gmail.com>
2019-10-13 16:59:56 -0700

    Op1, Op2, Opr obtained

--- frontend/js/drag_drop.js
+++ frontend/js/drag_drop.js
@@ -35,26 +35,19 @@ $ ( document ). ready(function() {
 
   cardspace = new CardSpace
 
-  // for (i=0; i < 10; i++){
-  //   card = new Card(i);
-  //   $("#cardPile").append(card.element)
-  //   cardspace.array.push(i)
-  // }
-
   cardspace.array = [0,1,2,3,4,5,6,7,8,9]
 
   opcardspace.array = ['+','-','x','/','=']
-  // cardspace.array = [0,1,2,3,4,5,6,7,8,9,'+','-','*','/']
 
   for(i=0; i<10; i++){
     card = new Card(cardspace.array[i])
     $("#cardPile").append(card.element)
   }
 
-  for(i=0; i<5; i++){
+  for(i=0; i<5; i++)
+  {
     card = new OpCard(opcardspace.array[i])
     $("#cardPile").append(card.element)
-    // console.log("inside");
   }
 
   console.log(cardspace.array);
@@ -71,41 +64,26 @@ $ ( document ). ready(function() {
   $( "#cardSlots" ).droppable({
     drop: function( event, ui ) {
 
-    var tokens=[];
-
-
-
     var value_num = $(ui.draggable).text();
     card_slot = cardspace.array[value_num]
 	  cardspace.array_member.push(value_num)
 
 	  var value_op = $(ui.draggable).text();
-	  // console.log(value_op);
-	  //card_slot = opcardspace.array[value_op]
+
 	  card_slot=value_op;
-	  // console.log(card_slot);
 	  opcardspace.array_member.push(value_op)
 	  $("#cardSlots").append("<div class='operator'>" + card_slot + "</div>")
 
-	  // console.log(card_slot)
-      var number_array_members = cardspace.array_member.length
-      // console.log(number_array_members)
-      console.log(cardspace.array_member)
-     /* if ( number_array_members <= 10 ) {
-        $("#cardSlots").append("<div class='number'>" + card_slot + "</div>")
+    var number_array_members = cardspace.array_member.length
+    console.log(cardspace.array_member)
 
-		/* To add and display in result column
-        value_sum = value_sum + card_slot
-        $("#total_sum").text(value_sum)
-      } */
-	  
 	  counter=0;
 	  var j=0;
 	  var k=0;
 	  var term1 = [];
 	  var term2 = [];
 	  var opr;
-		
+
 		for(i=0; i<number_array_members; i++)
 		{
 			counter=counter+1;
@@ -119,10 +97,10 @@ $ ( document ). ready(function() {
 				opr=cardspace.array_member[i];
 				break;
 			}
-			
+
 		}
-		
-		
+
+
 		for(i=counter;i<number_array_members;i++)
 		{
 			if(cardspace.array_member[i]!= "=")
@@ -131,14 +109,17 @@ $ ( document ). ready(function() {
 				k=k+1;
 			}
 		}
-		
-		console.log('Opr', opr);
+
 		console.log('T1', term1);
 		console.log('T2', term2);
-		
-      // op1=
-      // op2=
-      // opr=
+
+		op1 = term1.join("")
+    console.log('op1', op1);
+
+    console.log('Opr', opr);
+
+    op2 = term2.join("")
+    console.log('op2', op2);
 
 
     }
