6bb81daa8686dc709825f41aec1599cc4148b6d7
anushasingh30 <anusha2429@gmail.com>
2019-10-12 19:34:16 -0700

    Singleton Firbase Entry

--- tmp/db_handler/firebase_handler.py
+++ tmp/db_handler/firebase_handler.py
@@ -3,26 +3,42 @@ import time
 
 import requests
 
-def add_data(firebase_url, data, log_file=None):
-    try:
-        result = requests.post(firebase_url, data=json.dumps(data))
-        if log_file != None:
-            with open(log_file, 'a') as f:
-                f.write(time.strftime('%m-%d-%Y') + '|' +
-                        time.strftime('%H:%M:%S') +
-                        'Data Created: ' + json.dumps(data)+'\n')
-    except Exception as e:
-        if log_file != None:
-            with open(log_file, 'a') as f:
-                f.write(time.strftime('%m-%d-%Y') + '|' +
-                        time.strftime('%H:%M:%S') +
-                        'Exception:' + str(e) + '\n')
+class FirebaseEntryPoint:
+    __instance = None
+    FIREBASE_URL = 'https://mathub-ser515.firebaseio.com'
 
-def retrieve_data(firebase_url, username):
-    result = requests.get(firebase_url + '/{}.json'.format(username.split('.')[0]))
-    return result.json()
+    def __init__(self):
+        if FirebaseEntryPoint.__instance != None:
+            raise Exception('Cannot re-instantiate a singleton class.')
+        FirebaseEntryPoint.__instance = self
 
-def retrieve_password_from_fb_data(data):
-    data_to_list_of_tuples = list(data.items())
-    data_dict = data_to_list_of_tuples[0][1]
-    return data_dict['password']
+    @staticmethod
+    def create():
+        if FirebaseEntryPoint.__instance == None:
+            FirebaseEntryPoint()
+        return FirebaseEntryPoint.__instance
+
+    def add_data(self, firebase_url, data, log_file=None):
+        try:
+            result = requests.post(FirebaseEntryPoint.FIREBASE_URL + firebase_url, data=json.dumps(data))
+            if log_file != None:
+                with open(log_file, 'a') as f:
+                    f.write(time.strftime('%m-%d-%Y') + '|' +
+                            time.strftime('%H:%M:%S') +
+                            'Data Created: ' + json.dumps(data)+'\n')
+        except Exception as e:
+            if log_file != None:
+                with open(log_file, 'a') as f:
+                    f.write(time.strftime('%m-%d-%Y') + '|' +
+                            time.strftime('%H:%M:%S') +
+                            'Exception:' + str(e) + '\n')
+
+    def retrieve_data(self, firebase_url, username):
+        result = requests.get(FirebaseEntryPoint.FIREBASE_URL + firebase_url +
+                              '/{}.json'.format(username.split('.')[0]))
+        return result.json()
+
+    def retrieve_password_from_fb_data(self, data):
+        data_to_list_of_tuples = list(data.items())
+        data_dict = data_to_list_of_tuples[0][1]
+        return data_dict['password']
