43833a0798bf1193c9ade54f7bc3072ec1ff90cc
piyush2896 <piyushmalhotra28@gmail.com>
2019-10-18 11:41:14 -0700

    Give access from actiors import Student (or Teacher)

--- tmp/actors/__init__.py
+++ tmp/actors/__init__.py
@@ -3,3 +3,6 @@ All the actors, which are in contact with the web-interface, comes under this pa
 
 Every actor must inherit from the Abstract Base Class -> actors.client.BaseClient
 """
+
+from .__student import Student
+from .__teacher import Teacher
