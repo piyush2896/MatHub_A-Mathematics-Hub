a33769945b186baf02f5f27453d28d526b52ad15
anushasingh30 <anusha2429@gmail.com>
2019-10-29 12:54:34 -0700

    JS for create_user template

---
+++ tmp/static/js/create_user.js
@@ -0,0 +1,50 @@
+function studentDataForm(){
+    element = "<div class='row'><div class='form-group col-xs-6 col-xs-offset-3'>\
+        <label for='grade'>Grade</label>\
+        <select id='grade' class='form-control' name='grade'>";
+    for(i = 1; i <= 12; i++){
+        element += "<option value='"+ i + "'>" + i + "</option>";
+    }
+    element += "</select></div></div>";
+    return element;
+}
+
+function parentDataForm(){
+    element = "<div class='row'><div class='form-group col-xs-6 col-xs-offset-3'>\
+        <label for='stud-id'>Student ID</label>\
+        <input type='text' class='form-control' id='stud-id' name='stud-id' placeholder='Student ID'>";
+    element += "</div></div>";
+    return element;
+}
+
+function teacherDataForm(){
+    element = "<div class='row'><div class='form-group col-xs-6 col-xs-offset-3'>\
+        <label for='grade'>Grade</label>\
+        <select id='grade' class='form-control' multiple>";
+    for(i = 1; i <= 12; i++){
+        element += "<option value='"+ i + "'>" + i + "</option>";
+    }
+    element += "</select></div></div>";
+    return element;
+}
+
+$(document).ready(function(){    
+    
+    function updateExtraData(data){
+        $('#extra-details').html('');
+        $('#extra-details').html(data);
+    }
+
+    $('#usertype').change(function(){
+        var usertype = $('#usertype option:selected').text();
+        if(usertype == 'Student'){
+            updateExtraData(studentDataForm());
+        }else if(usertype == 'Teacher'){
+            updateExtraData(teacherDataForm());
+        }else if(usertype == 'Parent'){
+            updateExtraData(parentDataForm());
+        }else{
+            updateExtraData('');
+        }
+    });
+});
