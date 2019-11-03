8ec0add977c21d22909d0998d57061f2fbfbab94
piyush2896 <piyushmalhotra28@gmail.com>
2019-11-02 20:15:49 -0700

    Complete Refactor Flask Entry point

--- tmp/app.py
+++ tmp/app.py
@@ -7,8 +7,37 @@ import actors
 
 app = flask.Flask(__name__)
 
+def __get_url_from_usetype(usertype):
+    usertype_2_dburl = {
+        actors.STUDENT: actors.Student.DB_URL,
+        actors.ADMIN: actors.Admin.DB_URL,
+        actors.TEACHER: actors.Teacher.DB_URL,
+        actors.PARENT: actors.Parent.DB_URL
+    }
+    return usertype_2_dburl[usertype]
+
+def __get_user_data(username, usertype):
+    fb_handler = fb_handle.FirebaseEntryPoint.create()
+    data = fb_handler.retrieve_data(
+        __get_url_from_usetype(usertype), username)
+
+    return data
+
+def __login_helper(usertype, username, password_candidate):
+    fb_handler = fb_handle.FirebaseEntryPoint.create()
+    fb_url = __get_url_from_usetype(usertype)
+
+    return login_helper.verify(fb_url, username, password_candidate)
+
+def __login_count_incrementer(usertype, username):
+    fb_handler = fb_handle.FirebaseEntryPoint.create()
+    fb_url = __get_url_from_usetype(usertype)
+
+    fb_handler.increment_login_count(fb_url, username)
+
 @app.route('/')
 def index():
+    fb_handle.FirebaseEntryPoint.create()
     return flask.render_template('index.html')
 
 @app.route('/boards')
@@ -19,23 +48,13 @@ def boards():
 def arithmetic_board():
     return flask.render_template('arith.html')
 
-def __login_helper(usertype, username, password_candidate):
-    fb_handler = fb_handle.FirebaseEntryPoint.create()
-
-    if usertype == 'student':
-        fb_url = actors.Student.DB_URL
-    elif usertype == 'teacher':
-        fb_url = actors.Teacher.DB_URL
-    elif usertype == 'parent':
-        fb_url = actors.Parent.DB_URL
-    else:
-        fb_url = actors.Admin.DB_URL
-
-    return login_helper.verify(fb_url, username, password_candidate)
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'logged_in' in flask.session and flask.session['logged_in']:
+        if 'usertype' in flask.session and flask.session['usertype'] == actors.ADMIN:
+            return flask.redirect(flask.url_for('create_user'))
+        return flask.redirect(flask.url_for('boards'))
+
     if flask.request.method == 'POST':
         usertype = flask.request.form['optionRadios']
         username = flask.request.form['email']
@@ -47,6 +66,14 @@ def login():
             flask.session['logged_in'] = True
             flask.session['username'] = username
             flask.session['usertype'] = usertype
+
+            data = __get_user_data(username, usertype)
+            data = list(data.items())[0][1]
+            data['usertype'] = usertype
+
+            __login_count_incrementer(usertype, username)
+
+            flask.session['client'] = data
             return flask.redirect(flask.url_for('boards')) # TODO: Discuss
         else:
             flask.flash(error_if_any, 'danger')
@@ -55,17 +82,30 @@ def login():
 
 @app.route('/admin-login', methods=['GET', 'POST'])
 def admin_login():
+    if 'logged_in' in flask.session and flask.session['logged_in']:
+        if 'usertype' in flask.session and flask.session['usertype'] == actors.ADMIN:
+            return flask.redirect(flask.url_for('create_user'))
+        return flask.redirect(flask.url_for('boards'))
+
     if flask.request.method == 'POST':
         username = flask.request.form['email']
         password_candidate = flask.request.form['pwd']
 
         is_verified, error_if_any = __login_helper(
-            'admin', username, password_candidate)
+            actors.ADMIN, username, password_candidate)
         if is_verified:
             flask.session['logged_in'] = True
             flask.session['username'] = username
-            flask.session['usertype'] = 'admin'
-            return flask.redirect(flask.url_for('boards')) # TODO: Discuss
+            flask.session['usertype'] = actors.ADMIN
+
+            data = __get_user_data(username, actors.ADMIN)
+            data = list(data.items())[0][1]
+            data['usertype'] = actors.ADMIN
+
+            __login_count_incrementer(actors.ADMIN, username)
+
+            flask.session['client'] = data
+            return flask.redirect(flask.url_for('create_user')) # TODO: Discuss
         else:
             flask.flash(error_if_any, 'danger')
 
@@ -73,7 +113,46 @@ def admin_login():
 
 @app.route('/create-user', methods=['GET', 'POST'])
 def create_user():
-    if flask.session['logged_in'] and flask.session['usertype'] == 'admin':
+    if flask.session['logged_in'] and flask.session['usertype'] == actors.ADMIN:
+        if flask.request.method == 'POST':
+            username = flask.request.form['username']
+            usertype = flask.request.form['usertype']
+            name = flask.request.form['name']
+
+            data = {
+                'username': username,
+                'name': name,
+                'login_count': 0
+            }
+
+            if usertype == actors.STUDENT:
+                data['grade'] = flask.request.form['grade']
+                data['id'] = '{}'.format(actors.STUDENT_ID_PREFIX)
+            elif usertype == actors.TEACHER:
+                data['grades'] = flask.request.form['grades']
+                data['id'] = '{}'.format(actors.TEACHER_ID_PREFIX)
+            elif usertype == actors.PARENT:
+                data['stud_id'] = flask.request.form['stud-id']
+                data['id'] = '{}'.format(actors.PARENT_ID_PREFIX)
+            else:
+                data['id'] = '{}'.format(actors.ADMIN_ID_PREFIX)
+
+            fb_url = __get_url_from_usetype(usertype)
+            fb_handler = fb_handle.FirebaseEntryPoint.create()
+            
+            if data['id'] == None:
+                data['id'] += str(actors.START_ID)
+                fb_handler.set_id(fb_url, actors.START_ID)
+            else:
+                data['id'] += str(fb_handler.update_id(fb_url))
+
+            password = login_helper.default_password_generator(username, data['id'])
+            data['password'] = sha256_crypt.encrypt(password)
+            print(password)
+
+            actors.Admin.create_user(fb_url, data)
+            
+            flask.flash('User Created!', 'success')
         return flask.render_template('create_user.html')
     else:
         return flask.redirect(flask.url_for('login'))
