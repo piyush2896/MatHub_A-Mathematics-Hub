b38c1fc5750329f95297904d313c71b70ea713e6
piyush2896 <piyushmalhotra28@gmail.com>
2019-11-08 11:33:10 -0700

    Error handling in API

--- backend/app.py
+++ backend/app.py
@@ -18,12 +18,27 @@ def basic_arithmetic_ops():
     try:
         arithmetic_helper(data)
     except KeyError as e:
-        return flask.jsonify({'result': '{}'.format(e)})
-    except:
-        return flask.jsonify({'result': 'Error'}) 
-    implementor = basic_ops.OperatorImplementor.create()
-    result = implementor.do_op(data)
-    return flask.jsonify({'result': result})
+        return flask.jsonify({
+            'result': '{}'.format(e),
+            'success': False
+        })
+    except Exception as e:
+        return flask.jsonify({
+            'result': 'Error: {}'.format(e),
+            'success': False
+        }) 
+    try:
+        implementor = basic_ops.OperatorImplementor.create()
+        result = implementor.do_op(data)
+        return flask.jsonify({
+            'result': result,
+            'success': True
+        })
+    except Exception as e:
+        return flask.jsonify({
+            'result': 'Error: {}'.format(e),
+            'success': False
+        })
 
 @app.after_request
 def after_request(response):
@@ -31,6 +46,13 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
+
+@app.route('/get-ops', methods=['GET'])
+def get_ops():
+    data = flask.request.json
+    grade = data['grade']
+    # TODO: Send ops json
+    return flask.jsonify({"result": "TO BE IMPLEMENTED"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
