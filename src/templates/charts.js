274e6cd58f40113078f5c58564d769bb86e98dcf
san1197 <sandya7c@gmail.com>
2019-11-13 21:02:26 -0700

    New chart created

---
+++ src/templates/charts.js
@@ -0,0 +1,18 @@
+
+var student_no=[1,2,3,4,5,6,7,8,9,10]
+//For drawing lines
+var grades = [100,95,43,56,70,23,45,6,90,18]
+
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: student_no,
+    datasets: [
+      { 
+        data: grades
+      }
+    ]
+  }
+});
