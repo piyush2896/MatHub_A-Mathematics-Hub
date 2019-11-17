da1582e3c4c3bbe7e28712dc12121c66f788f949
san1197 <sandya7c@gmail.com>
2019-11-17 14:16:24 -0700

    Path changed to studprofile

--- src/app.py
+++ src/app.py
@@ -49,7 +49,7 @@ def boards():
 @app.route('/profile')
 def profile():
     if 'logged_in' in flask.session and flask.session['logged_in']:
-        return flask.render_template('profile.html')
+        return flask.render_template('studprofile.html')
     return flask.redirect(flask.url_for('login'))
 
 @app.route('/charts')
