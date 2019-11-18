59311ac264481e26fde1b5f8ddc16ffeba6b8ca9
Amudhan <amudhanmanisekaran@gmail.com>
2019-11-17 22:30:59 -0700

    Student assignment table added

--- src/static/js/studentprofile.js
+++ src/static/js/studentprofile.js
@@ -1,1 +1,25 @@
 
+$(document).ready(function () {
+console.log("Inside func");
+  var table = document.getElementById("AssignmentTable");
+
+var row = table.insertRow(1);
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+
+cell1.innerHTML = "1";
+cell2.innerHTML = "Assignment 1";
+cell3.innerHTML = "11/29/2019";
+
+var row = table.insertRow(2);
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+
+cell1.innerHTML = "2";
+cell2.innerHTML = "Assignment 2";
+cell3.innerHTML = "12/05/2019";
+
+
+ });
