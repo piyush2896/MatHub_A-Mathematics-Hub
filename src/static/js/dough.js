b996c09e953b103964bc28256d8d546ff41ce606
san1197 <sandya7c@gmail.com>
2019-11-28 16:45:36 -0700

    Chart relocated to dough.js

--- src/static/js/dough.js
+++ src/static/js/dough.js
@@ -1,22 +1,15 @@
-// Author - Sandya Manoharan
-// Date Modified - 11/25/19
-
-Chart.defaults.global.legend.display = false;
-var student_no=["S101","S102","S103","S104","S105"]
-
-var grades = [100,95,43,56,70]
-
+var assgn_no = ["A1","A2","A3"]
+var completion = [90,23,70]
 var ctx = document.getElementById("myChart");
 
-
 var myChart = new Chart(ctx, {
-  type: 'bar',
+  type: 'pie',
   data: {
-    labels: student_no,
+    labels: assgn_no,
     display: false,
     datasets: [
       {
-        data: grades,
+        data: completion,
         borderColor: "white",
         backgroundColor:"#213534",
         pointBackgroundColor: "red",
@@ -30,6 +23,3 @@ var myChart = new Chart(ctx, {
   
 
 });
-
-
-
